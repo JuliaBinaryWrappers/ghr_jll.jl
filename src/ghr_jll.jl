c9566c3607a0b27e75272ec852ddb9caad1e6489
jlbuild <juliabuildbot@gmail.com>
2020-10-18 22:47:57 +0000

    ghr_jll build 0.13.0+1

--- src/ghr_jll.jl
+++ src/ghr_jll.jl
@@ -1,31 +1,9 @@
-module ghr_jll
-using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
-import Base: UUID
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule ghr_jll
+using Base
+using Base: UUID
+import JLLWrappers
 
-# We put these inter-JLL-package API values here so that they are always defined, even if there
-# is no underlying wrapper held within this JLL package.
-const PATH_list = String[]
-const LIBPATH_list = String[]
-
-# Load Artifacts.toml file
-artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
-
-# Extract all platforms
-artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("07c12ed4-43bc-5495-8a2a-d5838ef8d533"))
-platforms = [Pkg.Artifacts.unpack_platform(e, "ghr", artifacts_toml) for e in artifacts["ghr"]]
-
-# Filter platforms based on what wrappers we've generated on-disk
-platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
-
-# From the available options, choose the best platform
-best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
-
-# Silently fail if there's no binaries for this platform
-if best_platform === nothing
-    @debug("Unable to load ghr; unsupported platform $(triplet(platform_key_abi()))")
-else
-    # Load the appropriate wrapper
-    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
-end
-
+JLLWrappers.@generate_main_file_header("ghr")
+JLLWrappers.@generate_main_file("ghr", UUID("07c12ed4-43bc-5495-8a2a-d5838ef8d533"))
 end  # module ghr_jll
